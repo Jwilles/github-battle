eb45ff2cc9420dff830b8124b037f04ae6e3b59c
John <johnwilles@gmail.com>
2017-04-02 22:48:51 -0400

    Add server

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,7 +9,7 @@ var Home = React.createClass({
     return (
       <MainContainer>
         <h1>Github Battle</h1>
-        <p className='lead'>Some Motto</p>
+        <p className='lead'>Battle your friends!</p>
         <Link to='/playerOne'>
           <button type="button" className="btn btn-lg btn-success">Get Started</button>
         </Link>
